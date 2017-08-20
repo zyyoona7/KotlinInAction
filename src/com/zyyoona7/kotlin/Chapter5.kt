7328d1c665da1b910f00d0f98d765b01f47830ad
zyyoona7 <19671746@qq.com>
2017-08-20 16:49:24 +0800

    chapter 5

--- src/com/zyyoona7/kotlin/Chapter5.kt
+++ src/com/zyyoona7/kotlin/Chapter5.kt
@@ -8,10 +8,44 @@ fun main(args: Array<String>) {
     val maxAge3 = list.maxBy(Person::age)
     val maxAge4 = list.maxBy { p -> p.age }
     println("$maxAge,$maxAge1,$maxAge2,$maxAge3,$maxAge4")
+
+
+    val chapter5 = Chapter5()
+    println(chapter5.alphabet1())
+    println(chapter5.alphabet2())
+    println(chapter5.alphabet3())
 }
 
 class Chapter5 {
 
+    //with，apply
+    fun alphabet1(): String {
+        val strBuilder = StringBuilder()
+        for (str in 'A'..'Z') {
+            strBuilder.append(str)
+        }
+        strBuilder.append("\nalphabet1 print finished.")
+        return strBuilder.toString()
+    }
+
+
+    fun alphabet2():String {
+        val strBuilder = StringBuilder()
+        return with(strBuilder) {
+            for (str in 'A'..'Z') {
+                append(str)
+            }
+            append("\nalphabet2 print finished.")
+            toString()
+        }
+    }
+
+    fun alphabet3() = StringBuilder().apply {
+        for (str in 'A'..'Z') {
+            append(str)
+        }
+        append("\nalphabet3 print finished.")
+    }.toString()
 }
 
 data class Person(val name: String, val age: Int)
