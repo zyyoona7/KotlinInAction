8c8c8fe0a8afd603ca5a51700e07990dbe19cf14
zyyoona7 <19671746@qq.com>
2017-08-23 17:40:22 +0800

    chapter 11

---
+++ src/com/zyyoona7/kotlin/Chapter10.kt
@@ -0,0 +1,32 @@
+package com.zyyoona7.kotlin
+
+import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
+
+fun main(args: Array<String>) {
+    val chapter10 = Chapter10()
+    val kClass = chapter10::class
+    val properties = chapter10.javaClass.kotlin.memberProperties
+    kClass.memberProperties.forEach { println(it.name) }
+    val jsonName = kClass.memberProperties.filter { it.findAnnotation<JsonName>() != null }
+    val prop = jsonName.firstOrNull()
+
+    val clazz = Chapter10::class
+    val c1 = clazz.objectInstance ?: clazz.createInstance()
+    c1.name = "123"
+    c1.printHello()
+    println(c1.name)
+}
+
+class Chapter10 {
+    @JsonName("hello")
+    var name: String = "zhang"
+
+    fun printHello() {
+        println("Hello!")
+    }
+}
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class JsonName(val name: String)
---
+++ src/com/zyyoona7/kotlin/Chapter11.kt
@@ -0,0 +1,45 @@
+package com.zyyoona7.kotlin
+
+fun main(args: Array<String>) {
+    val str1 = buildString {
+        it.append("Hello ")
+        it.append("World")
+        it.append("!")
+    }
+    println(str1)
+
+    val str2 = buildString1 {
+        append("Hello ")
+        append("lambda")
+    }
+
+    println(str2)
+
+    val str3 = buildString2 {
+        println(it)
+        append("你 ")
+        append("好")
+    }
+    println(str3)
+}
+
+fun buildString(buildAction: (StringBuilder) -> Unit): String {
+    val sb = StringBuilder()
+    buildAction(sb)
+    return sb.toString()
+}
+
+//带接受者的lambda
+fun buildString1(buildAction: StringBuilder.() -> Unit): String {
+    val sb = StringBuilder()
+    sb.buildAction()
+    return sb.toString()
+}
+
+fun buildString2(buildAction: StringBuilder.(String) -> Unit): String {
+    return StringBuilder().apply { buildAction("Hi!") }.toString()
+}
+
+class Chapter11 {
+
+}
